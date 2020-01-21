de5aafa696e1dcbe83fb7f6c858dc5eafd06b417
brandonrodriguez6 <37849270+brandonrodriguez6@users.noreply.github.com>
2020-01-21 14:18:06 -0800

    Add files via upload

---
+++ melody.rb
@@ -0,0 +1,40 @@
+# Ben E. King: Stand By Me
+use_bpm 112
+
+play :Gs4
+sleep 1
+play :E4
+sleep 1
+play :C4
+sleep 1.25
+play :E4
+sleep 1
+play :Gs4
+sleep 1
+play :c5
+sleep 1
+play :E5
+sleep 1.25
+play :D5
+sleep 1
+play :E4
+sleep 1
+play :Fs4
+sleep 1
+play :G4
+sleep 1
+play :G4
+sleep 0.5
+play :G4
+sleep 1
+play :E5
+sleep 1
+play :D5
+sleep 0.75
+play :C5
+sleep 1
+play :B4
+sleep 1
+
+
+
